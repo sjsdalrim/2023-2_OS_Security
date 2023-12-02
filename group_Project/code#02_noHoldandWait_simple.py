ba8e966d08cc9e2126662d9928ba627d24f97557
Junsang Seo <72577353+sjsdalrim@users.noreply.github.com>
2023-12-03 06:35:01 +0900

    Create code#02_noHoldandWait_simple.py

---
+++ group_Project/code#02_noHoldandWait_simple.py
@@ -0,0 +1,66 @@
+
+from threading import Thread, Lock
+
+import time
+import random
+
+
+counter = 0
+
+
+def worker_1(_lock_P, _lock_1, _lock_2, _num):
+    for _ in range(_num):
+        
+        while True:
+            if (_lock_P.acquire() == True):     ## wait Lock
+                break
+        
+        _lock_1.acquire()
+        _lock_2.acquire()
+        
+        ## what you want to do in worker1 Thread
+        global counter
+        counter += 1
+        ##
+
+        _lock_1.release()
+        _lock_2.release()
+        _lock_P.release()
+        time.sleep(random.random())
+
+
+def worker_2(_lock_P, _lock_1, _lock_2, _num):
+    for _ in range(_num):
+        
+        while True:
+            if (_lock_P.acquire() == True):     ## wait Lock
+                break
+        
+        _lock_2.acquire()
+        _lock_1.acquire()
+        
+        ## what you want to do in worker1 Thread
+        global counter
+        counter += 1
+        ##
+
+        _lock_2.release()
+        _lock_1.release()
+        _lock_P.release()
+        time.sleep(random.random())
+
+
+lock_Prevent = Lock()
+lock_X = Lock()
+lock_Y = Lock()
+
+t1 = Thread(target=worker_1, args=(lock_Prevent, lock_X, lock_Y, 100))
+t2 = Thread(target=worker_2, args=(lock_Prevent, lock_X, lock_Y, 100))
+
+
+t1.start(), t2.start()
+
+t1.join(), t2.join()
+
+
+print('result = ' + str(counter))
