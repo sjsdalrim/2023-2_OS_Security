bd067fb2d9949e7418c1f5969baa979d15cae1ed
Junsang Seo <72577353+sjsdalrim@users.noreply.github.com>
2023-12-06 18:32:13 +0900

    Update code#01_Preemption.py

--- group_Project/code#01_Preemption.py
+++ group_Project/code#01_Preemption.py
@@ -10,60 +10,62 @@ counter = 0
 
 def worker_1(_lock_1, _lock_2, _num):
     for _ in range(_num):
-
-        time.sleep(random.random())
-
-        _lock_1.acquire()
-        print('worker_1 acquired lock_X')
-
-        time.sleep(random.random())
-       
-            ## if Y locked, release X
-        if (_lock_2.locked() == True):
-            _lock_1.release()
-            print('worker_1 released lock_X')
-            print('                              [  FAIL ] worker_1 release Lock')
-        else:
-            _lock_2.acquire()
-            print('worker_1 acquired lock_Y')
-            
-            ## what you want to do in worker1 Thread
-            global counter
-            counter += 1
-            ##
-
-            _lock_1.release()
-            _lock_2.release()
-            print('                              [SUCCESS] worker_1 complete')
+        while True:
+            time.sleep(random.random())
+    
+            _lock_1.acquire()
+            print('worker_1 acquired lock_X')
+    
+            time.sleep(random.random())
+           
+                ## if Y locked, release X
+            if (_lock_2.locked() == True):
+                _lock_1.release()
+                print('worker_1 released lock_X')
+                print('[FAIL] Retry worker1')
+            else:
+                _lock_2.acquire()
+                print('worker_1 acquired lock_Y')
+                
+                ## what you want to do in worker1 Thread
+                global counter
+                counter += 1
+                ##
+    
+                _lock_1.release()
+                _lock_2.release()
+                print('[SUCCESS] worker_1 complete')
+                break
 
 
 def worker_2(_lock_1, _lock_2, _num):
     for _ in range(_num):
 
-        time.sleep(random.random())
-
-        _lock_2.acquire()
-        print('worker_2 acquired lock_Y')
-        
-        time.sleep(random.random())
-        
-            ## if X locked, release Y
-        if (_lock_1.locked() == True):
-            _lock_2.release()
-            print('worker_2 released lock_Y')
-            print('                              [  FAIL ] worker_2 release Lock')
-        else:
-            _lock_1.acquire()
-            print('worker_2 acquired lock_X')
-
-            ## what you want to do in worker2 Thread
-            global counter
-            counter += 1
-            ##
-
-            _lock_1.release()
-            _lock_2.release()
-            print('                              [SUCCESS] worker_2 complete')
+            time.sleep(random.random())
+    
+            _lock_2.acquire()
+            print('worker_2 acquired lock_Y')
+            
+            time.sleep(random.random())
+            
+                ## if X locked, release Y
+            if (_lock_1.locked() == True):
+                _lock_2.release()
+                print('worker_2 released lock_Y')
+                print('[FAIL] Retry worker2')
+            else:
+                _lock_1.acquire()
+                print('worker_2 acquired lock_X')
+    
+                ## what you want to do in worker2 Thread
+                global counter
+                counter += 1
+                ##
+    
+                _lock_1.release()
+                _lock_2.release()
+                print('[SUCCESS] worker_2 complete')
+                break
 
 
 lock_X = Lock()
